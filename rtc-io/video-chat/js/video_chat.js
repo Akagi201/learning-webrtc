82306e2938c0ab28c4fb9262243b7196299198d2
Akagi201 <akagi201@gmail.com>
2016-10-13 16:19:59 +0800

    rtc-io: add video-chat

---
+++ rtc-io/video-chat/js/video_chat.js
@@ -0,0 +1,42 @@
+'use strict'
+
+// Set RTC options.
+var rtcOpts = {
+    room: 'test-room',
+    signaller: 'http://10.0.2.246:3000'
+};
+// call RTC module
+var rtc = RTC(rtcOpts);
+// A div element to show our local video stream
+var localVideo = document.getElementById('l-video');
+// A div element to show our remote video streams
+var remoteVideo = document.getElementById('r-video');
+// A contenteditable element to show our messages
+var messageWindow = document.getElementById('messages');
+
+// Bind to events happening on the data channel
+function bindDataChannelEvents(id, channel, attributes, connection) {
+
+    // Receive message
+    channel.onmessage = function (evt) {
+        messageWindow.innerHTML = evt.data;
+    };
+
+    // Send message
+    messageWindow.onkeyup = function () {
+        channel.send(this.innerHTML);
+    };
+}
+
+// Start working with the established session
+function init(session) {
+    session.createDataChannel('chat');
+    session.on('channel:opened:chat', bindDataChannelEvents);
+}
+
+// Display local and remote video streams
+localVideo.appendChild(rtc.local);
+remoteVideo.appendChild(rtc.remote);
+
+// Detect when RTC has established a session
+rtc.on('ready', init);
